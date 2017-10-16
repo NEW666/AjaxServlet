a509c80d32f0167368c81cb68895a70f2d681dae
NEW666 <2209325428@qq.com>
2017-10-16 16:26:11 +0800

    有中文

--- AjaxServlet/src/com/AjaxServlet/test/ServletJson.java
+++ AjaxServlet/src/com/AjaxServlet/test/ServletJson.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-//˵�����Servlet��û�����кŵ�  
+//˵�����Servlet中文
 @SuppressWarnings("serial")  
 //˵�����Servlet��������jsonRequest�����ַ��/jsonRequest������web.xml��������һ����  
 @WebServlet(name = "jsonRequest", urlPatterns = { "/jsonRequest" })  
